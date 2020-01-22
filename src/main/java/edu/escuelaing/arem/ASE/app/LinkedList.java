75fd836511eb798d7ee2df400083f0a092b086b0
Andres Villamil <andresvilla990223@gmail.com>
2020-01-21 22:39:57 -0500

    commented

--- src/main/java/edu/escuelaing/arem/ASE/app/LinkedList.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/LinkedList.java
@@ -1,11 +1,13 @@
 package edu.escuelaing.arem.ASE.app;
 
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * 
- * LinkedList class
- * Is a list that consist of multiple Nodes connected.
+ * LinkedList class Is a genereic list that consist of multiple Nodes connected.
  */
-public class LinkedList<T> {
+public class LinkedList<T> implements Iterable<T> {
 
     private Node<T> head;
     private Node<T> tail;
@@ -19,9 +21,11 @@ public class LinkedList<T> {
     public LinkedList() {
         size = 0;
     }
+
     /**
+     * Constructor that recieves 1 data set and converts it to a node
      * 
-     * @param data
+     * @param data the value of the first node
      */
     public LinkedList(T data) {
         type = data.getClass().getName();
@@ -31,26 +35,77 @@ public class LinkedList<T> {
         size = 1;
     }
 
-    public boolean add(T data) {
-        Node<T> n = new Node<T>(data, null, tail);
+    /**
+     * Constructor that receives a T data array
+     * 
+     * @param dat array of data T
+     */
+    public LinkedList(T[] dat) {
 
-        if (size == 0) {
+        type = dat.getClass().getName();
 
-            type = data.getClass().getName();
-            head = n;
-        } else {
-            getNode(size - 1).setNext(n);
-            n.setPrev(tail);
+        Node<T> pre = null;
+        Node<T> nex = null;
+        int s = 0;
+        for (T x : dat) {
+            Node<T> n = new Node<T>(x, nex, pre);
+            if (s == 0) {
+                head = n;
+            }
+            if (!(s == 0)) {
+                pre.setNext(n);
+            }
+            pre = n;
+            if (s == dat.length) {
+                tail = n;
+            }
+            s++;
 
         }
-        tail = n;
-        size++;
 
-        return true;
+        size = s;
     }
 
-    public void add(int index, T data) {
-        if (!(index < 0 || index >= size)) {
+    /**
+     * Method add a single T data element
+     * 
+     * @param data T elemtn to add
+     * @return boolean indicating if it was added
+     * @throws Exception if the data type added doesn't match with the linkedlist data type
+     */
+    public boolean add(T data) throws Exception {
+
+        if (type == data.getClass().getName()) {
+
+            Node<T> n = new Node<T>(data, null, tail);
+
+            if (size == 0) {
+
+                type = data.getClass().getName();
+                head = n;
+            } else {
+                getNode(size - 1).setNext(n);
+                n.setPrev(tail);
+
+            }
+            tail = n;
+            size++;
+
+            return true;
+        } else {
+            throw new Exception("Data type is not compatible with curretn LinkedList");
+        }
+    }
+
+    /**
+     * Method adds an element T in poscision index
+     * 
+     * @param index poscision where to add the data
+     * @param data  T to be added
+     * @throws Exception if the data type added doesn't match with the linkedlist data type
+     */
+    public void add(int index, T data) throws Exception {
+        if (!(index < 0 || index >= size || type == data.getClass().getName())) {
             Node<T> Iprev = getNode(index - 1);
             Node<T> Inext = getNode(index);
             Node<T> n = new Node<T>(data, Inext, Iprev);
@@ -58,12 +113,48 @@ public class LinkedList<T> {
             Inext.setPrev(n);
             size++;
         } else {
-            // exception
+            throw new Exception("Data type is not compatible with curretn LinkedList");
         }
 
     }
 
-    public void clear() {        
+    /**
+     * Method adds to the linkedlist elements of an T data array
+     * @param dat T data array
+     * @return boolean indicating if the array was added
+     * @throws Exception if the data type added doesn't match with the linkedlist data type
+     */
+    public boolean addAll(T[] dat) throws Exception {
+        if (type == dat.getClass().getName()) {
+
+            Node<T> pre = tail;
+            Node<T> nex = null;
+            int s = size;
+            for (T x : dat) {
+                Node<T> n = new Node<T>(x, nex, pre);
+                if (s == 0) {
+                    head = n;
+                }
+                if (!(s == 0)) {
+                    pre.setNext(n);
+                }
+                pre = n;
+                if (s == dat.length) {
+                    tail = n;
+                }
+                s++;
+            }
+
+            size = s;
+            return true;
+        }
+        else  throw new Exception("Data type is not compatible with curretn LinkedList");
+    }
+
+    /**
+     *  Method clears the linked list
+     */
+    public void clear() {
         head = null;
         tail = null;
         size = 0;
@@ -71,18 +162,34 @@ public class LinkedList<T> {
 
     }
 
+    /**
+     * Methos that search for an specific value on the linkedlist  
+     * @param data Value to search on the linkedlist
+     * @return boolean indicating if the value is present on the linkedlist
+     */
     public boolean contains(T data) {
-        Node<T> sHead = head;
-        while (sHead.getNext() != null) {
-            if (sHead.getNext().getValue().equals(data)) {
-                return true;
-            } else
-                sHead = sHead.getNext();
 
+        if (type == data.getClass().getName()) {
+
+            Node<T> sHead = head;
+            while (sHead.getNext() != null) {
+                if (sHead.getNext().getValue().equals(data)) {
+                    return true;
+                } else
+                    sHead = sHead.getNext();
+
+            }
+            return false;
+        } else {
+            return false;
         }
-        return false;
     }
 
+    /**
+     * method returns an specific value based on an index
+     * @param index int that indicates which value to return
+     * @return T or null if the index is wrong
+     */
     public T get(int index) {
         if (index >= size || index < 0) {
             return null;
@@ -92,10 +199,16 @@ public class LinkedList<T> {
         }
     }
 
+    /**
+     * Method return the node in an specific location
+     * @param index position of the node to extract
+     * @return the node on the location or null if the location is invalid
+     */
     private Node<T> getNode(int index) {
         int m = size / 2;
         int it;
-        Node<T> F;
+        Node<T> F = null;
+        
 
         if (index <= m) {
             F = head;
@@ -118,7 +231,15 @@ public class LinkedList<T> {
 
     }
 
+    /**
+     * Method returns the index of an specific value
+     * @param data the value to be searched
+     * @return the index of the node where the value is stored, or -1 if it isn't found
+     */
     public int indexOf(T data) {
+
+        type = data.getClass().getName();
+        
         Node<T> sHead = head;
         int index = 0;
 
@@ -133,7 +254,11 @@ public class LinkedList<T> {
         return -1;
 
     }
-
+    
+    /**
+     * Method return a boolean indicating if the list is empty
+     * @return boolean
+     */
     public boolean isEmpty() {
         if (size != 0) {
             return false;
@@ -141,34 +266,50 @@ public class LinkedList<T> {
             return true;
     }
 
-    public boolean remove(T data) {
-        int exist = indexOf(data);
-        if (exist != (-1)) {
+    /**
+     * Method remove a node based on an specific value
+     * @param data data that the node to be deleted contains
+     * @return boolean indicating if the node was deleted
+     * @throws Exception if the data type to be searched isn't the same as the linkedlist
+     */
+    public boolean remove(T data) throws Exception {
 
-            Node<T> Iprev = getNode(exist).getPrev();
-            Node<T> Inext = getNode(exist).getNext();
-            Node<T> n = getNode(exist);
+        if (type == data.getClass().getName()) {
 
-            if (exist == size - 1) {
-                tail = Iprev;
-                Iprev.setNext(Inext);
+            int exist = indexOf(data);
+            if (exist != (-1)) {
 
-            } else if (exist == 0) {
-                head = Inext;
-                Inext.setPrev(Iprev);
+                Node<T> Iprev = getNode(exist).getPrev();
+                Node<T> Inext = getNode(exist).getNext();
+                Node<T> n = getNode(exist);
 
-            } else {
-                Iprev.setNext(Inext);
-                Inext.setPrev(Iprev);
+                if (exist == size - 1) {
+                    tail = Iprev;
+                    Iprev.setNext(Inext);
+
+                } else if (exist == 0) {
+                    head = Inext;
+                    Inext.setPrev(Iprev);
+
+                } else {
+                    Iprev.setNext(Inext);
+                    Inext.setPrev(Iprev);
+                }
+                n.delete();
+                n = null;
+                return true;
+
             }
-            n.delete();
-            n = null;
-            return true;
-
-        }
-        return false;
+            return false;
+        } else
+            throw new Exception("Data type is not compatible with curretn LinkedList");
     }
 
+    /**
+     * Method removes a node based on its position
+     * @param index position of the node to be removed
+     * @return boolean indicating if the node was removed
+     */   
     public T remove(int index) {
 
         if (index < size && index > -1) {
@@ -196,10 +337,17 @@ public class LinkedList<T> {
         return null;
     }
 
+    /**
+     * Method returns the size of the linkedlist
+     * @return int the size of the linkedlist
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Method remove the head of the linkedlist
+     */
     public void removeHead() {
 
         Node<T> nHead = head.getNext();
@@ -210,6 +358,9 @@ public class LinkedList<T> {
 
     }
 
+    /**
+     * Method removes the tail of the linkedlist
+     */
     public void removeTail() {
         Node<T> nTail = tail.getPrev();
         if (!(nTail == null)) {
@@ -224,12 +375,26 @@ public class LinkedList<T> {
 
     }
 
+    /**
+     * Method returns the head of the linkedlist
+     * @return Node<T> the head of the linked list
+     */
     public Node<T> head() {
         return head;
     }
 
+     /**
+     * Method returns the tail of the linkedlist
+     * @return Node<T> the tail of the linked list
+     */
     public Node<T> tail() {
         return tail;
+    }
+
+    
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
